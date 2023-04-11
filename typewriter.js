acbd0df8075606414315e559b6cbeae96a40efbc
SirGraybon <graydonritchie@gmail.com>
2023-04-11 13:57:17 -0400

    implemented typewriter function

---
+++ typewriter.js
@@ -0,0 +1,24 @@
+
+const typewriter = function() {
+const sentence = "hello there from lighthouse labs";
+let delay = 50
+for (const char of sentence) {
+
+  setTimeout(() =>{
+
+    process.stdout.write(char)
+
+  }, delay);
+
+  delay += 50
+  
+  
+  
+}
+setTimeout(() => {
+  console.log("\n")
+}, delay)
+}
+
+
+typewriter()
